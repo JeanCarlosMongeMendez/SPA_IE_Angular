3e033677525530078476e6018ec62feb642d9bff
JeanCarlosMongeMendez <42584455+JeanCarlosMongeMendez@users.noreply.github.com>
2020-06-30 23:39:01 -0600

    project init
    empty project

---
+++ Spring/src/test/java/cr/ac/ucr/spa/SpaApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.ucr.spa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
