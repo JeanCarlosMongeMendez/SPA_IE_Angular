9a54c07b9ae60b93e47e26c259dc73ac766db21e
JeanCarlosMongeMendez <42584455+JeanCarlosMongeMendez@users.noreply.github.com>
2020-07-09 09:11:02 -0600

    model updated

---
+++ Angular/SPA-IE/src/app/model/News.ts
@@ -0,0 +1,9 @@
+export class News {
+    idNews: number;
+    username: string;
+    idUserCreator: number;
+    description: string;
+    documentation: string;
+    newsType: string;
+    image: string;
+}
